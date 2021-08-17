0b774cd133e62ff85dd831ec4dbb3b3e7c3c6025
Aniell4 <43622438+Aniell4@users.noreply.github.com>
2021-08-17 16:51:19 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# gelbooru-image-downloader
+downloads images from selected tags using pygelbooru
+
+just edit main.py line 12 with your gelbooru API_KEY and USER_ID 
+and be sure to have all the necessary requirements 
+( pip3 install requests pygelbooru asyncio )
---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+
+try:
+ from pygelbooru import Gelbooru
+except ImportError:
+ os.system('pip install pygelbooru')
+ from pygelbooru import Gelbooru
+
+try:
+ import aiosonic
+except ImportError:
+ os.system('pip install aiosonic')
+ import aiosonic
+
+import asyncio
+
+
+async def get_images(tag_list):
+    gelbooru = Gelbooru('API_KEY', 'USER_ID')
+    result = await gelbooru.search_posts(tags=tag_list, limit = 1000)
+    return result
+
+async def main():
+    tags = input("Enter Gelbooru Tags:")
+    tag_list = tags.split(' ')
+
+    result = await get_images(tag_list)
+
+    client = aiosonic.HTTPClient()
+    for image in result:
+        image_url = str(image)
+        filename = image_url.split("/")[-1]
+
+        r = await client.get(image_url)
+
+        if r.status_code == 200:
+            
+            newpath = str(tags) 
+            if not os.path.exists(newpath):
+                os.makedirs(newpath)
+            with open(newpath+'/'+filename,'wb') as f:
+                f.write(await r.content())
+                
+            print('Image sucessfully Downloaded: ',filename)
+        else:
+            print('Image Couldn\'t be retreived')
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()

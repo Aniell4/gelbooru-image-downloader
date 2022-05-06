bff1737ce92374d4c662a6d9a71b00b978bf3af5
Matthew Warren <mw@matthewwarren.dev>
2022-05-05 23:59:36 -0500

    Update code to not crash, use requests instead of aiosonic, retrieve tags, not be spammed with async, and provide more control over the number of images retrieved

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # gelbooru-image-downloader
 downloads images from selected tags using pygelbooru
 
-just edit main.py line 19 with your gelbooru API_KEY and USER_ID
+just edit main.py line 7 with your gelbooru API_KEY and USER_ID
 (you can find those at the bottom of https://gelbooru.com/index.php?page=account&s=options)
+
+## Required packages
+
+requests
+pygelbooru
--- main.py
+++ main.py
@@ -1,48 +1,64 @@
-import os
 import asyncio
+import os
+import requests
+from pygelbooru import Gelbooru
 
-try:
- from pygelbooru import Gelbooru
-except ImportError:
- os.system('pip install pygelbooru')
- from pygelbooru import Gelbooru
-
-try:
- import aiosonic
- from aiosonic.timeout import Timeouts
-except ImportError:
- os.system('pip install aiosonic')
- import aiosonic
- from aiosonic.timeout import Timeouts
-
-async def get_images(tag_list):
+def get_images(tag_list: list[str], num_images: int, page: int):
     gelbooru = Gelbooru('API_KEY', 'USER_ID')
-    return await gelbooru.search_posts(tags=tag_list, limit = 1000)
+    return asyncio.run(gelbooru.search_posts(tags=tag_list, limit=num_images, page=page))
 
-async def main():
+def main():
     tags = input("Enter Gelbooru Tags: ")
     tag_list = tags.split(' ')
-   
-    client = aiosonic.HTTPClient()
-    for image in await get_images(tag_list):
-        image_url = str(image)
-        filename = image_url.split("/")[-1]
+    num_images = int(input('Enter the number of images to download: '))
+    download(tags, tag_list, num_images)
 
-        r = await client.get(image_url, timeouts=Timeouts(request_timeout=1000))
+def download(save_dir: str, tag_list: list[str], num_images: int):
+    if num_images <= 0:
+        num_images = 1000
+        all_images = True
+    else:
+        all_images = False
 
-        if r.status_code == 200:
-            
-            newpath = str(tags) 
-            if not os.path.exists(newpath):
-                os.makedirs(newpath)
-            with open(newpath+'/'+filename,'wb') as f:
-                f.write(await r.content())
-                
-            print('Image sucessfully Downloaded: ',filename)
-        else:
-            print('Image Couldn\'t be retreived')
+    page_num = 0
+    num_per_page = min(1000, num_images)
+    total = 0
+    while all_images or total < num_images:
+        images = get_images(tag_list, num_per_page, page_num)
+        if not isinstance(images, list):
+            images = [images]
+        if len(images) == 0:
+            return
+
+        for image in images:
+            url = image.file_url
+            filename = image.filename
+            total += 1
+            print(f'[{total}] ', end='')
+
+            response = requests.get(url, stream=True)
+            if response.ok:
+                if not os.path.exists(save_dir):
+                    os.makedirs(save_dir)
+                file_path = os.path.join(save_dir, filename)
+                if not os.path.exists(file_path):
+                    with open(file_path, 'wb') as f:
+                        for block in response.iter_content(8192):
+                            if not block:
+                                break
+                            f.write(block)
+                    print(f'Image sucessfully downloaded: {filename}')
+                else:
+                    print(f'Image already downloaded: {filename}')
+
+                tag_file = f'{file_path}.tags'
+                if not os.path.exists(tag_file):
+                    with open(tag_file, 'w') as f:
+                        f.write('\n'.join(image.tags))
+            else:
+                print(f'Image could not be retreived: {filename}')
+
+        page_num += 1
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
+    main()
